b0f68603294f1c71c278b980d02af57eefc97930
Xavier Yao <xavieryao@live.cn>
2019-11-17 11:55:11 -0700

    regression model forward

--- reg_model.py
+++ reg_model.py
@@ -1,5 +1,6 @@
 import torch.nn.functional as F
 from torch import nn
+import torch
 from model import SavableModel
 
 
@@ -20,3 +21,25 @@ class EmbRegressionModel(SavableModel):
         self.shot_encoder = nn.TransforerEncoder(self.context_encoder_layer, num_layers)
 
         self.output = nn.Linear(embed_dim * num_contexts, embed_dim)
+
+    def forward(self, x: torch.Tensor):
+        # Input: B x C x L x N
+        # Output: N x L
+        batch_size, num_contexts, context_length, num_embeddings = x.shape
+
+        # context encoding
+        context_encodings = []
+        for c in range(num_contexts):
+            ctx = x[:, c].transpose(0, 1)  # L x B x N
+            ctx: torch.Tensor = self.context_encoder(ctx)  # L x B x N
+            ctx = ctx.permute(1, 2, 0)  # B x N x L
+            ctx = self.context_pool(ctx).squeeze()  # B x N x 1
+            context_encodings.append(ctx)
+        context_encodings = torch.stack(context_encodings)  # C x B x N
+
+        shot_encoding = self.shot_encoder(context_encodings)  # C x B x N
+        shot_encoding = shot_encoding.transpose(0, 1)  # B x C x N
+        shot_encoding = shot_encoding.reshape(batch_size, -1)  # B x N
+
+        output = self.output(shot_encoding)
+        return F.normalize(output, p=2, dim=-1)
